d3b0b6d32787675d2133d5cd0e20533cb8f54e3b
Kurstch <61703543+Kurstch@users.noreply.github.com>
2022-09-07 14:36:58 +0300

    Fix indentation guideline visual bug

--- lua/indent_blankline_setup.lua
+++ lua/indent_blankline_setup.lua
@@ -1,7 +1,7 @@
 vim.opt.list = true
 
 require('indent_blankline').setup {
-    space_char_blankline = '',
+    space_char_blankline = ' ',
     show_current_context = true,
     show_trailing_blankline_indent = false,
 }
