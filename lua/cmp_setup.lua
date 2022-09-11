33472d2341c616cda3056b649e702476cae0b2e4
Kurstch <61703543+Kurstch@users.noreply.github.com>
2022-09-11 13:26:40 +0300

    Fix missing value in cmp_setup.lua

--- lua/cmp_setup.lua
+++ lua/cmp_setup.lua
@@ -2,7 +2,7 @@ local cmp = require('cmp')
 
 cmp.setup({
     snippet = {
-        expand = function()
+        expand = function(args)
             vim.fn["vsnip#anonymous"](args.body)
         end,
     },
