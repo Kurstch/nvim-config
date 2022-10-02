abae00359dc97189a0736492474dc1627f4de5d2
Kurstch <61703543+Kurstch@users.noreply.github.com>
2022-10-02 21:38:08 +0300

    Update dashboard to display random ascii art in rainbow color

--- lua/config/dashboard.lua
+++ lua/config/dashboard.lua
@@ -1,27 +1,40 @@
 local dashboard = require('dashboard')
 
-dashboard.custom_header = {
-    [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀]],
-    [[⠀⠀⠀⠀⠀⠀⢀⡴⠋⠁⠀⠀⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠀]],
-    [[⠀⠀⠀⠀⢀⡴⠃⢀⡴⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀]],
-    [[⠀⠀⠀⠀⡾⠁⣠⠋⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀]],
-    [[⠀⠀⠀⣸⠁⢰⠃⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠀]],
-    [[⠀⠀⠀⡇⠀⡾⡀⠀⠀⠀⠀⣀⣹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀]],
-    [[⠀⠀⢸⠃⢀⣇⡈⠀⠀⠀⠀⠀⠀⢀⡑⢄⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇]],
-    [[⠀⠀⢸⠀⢻⡟⡻⢶⡆⠀⠀⠀⠀⡼⠟⡳⢿⣦⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇]],
-    [[⠀⠀⣸⠀⢸⠃⡇⢀⠇⠀⠀⠀⠀⠀⡼⠀⠀⠈⣿⡗⠂⠀⠀⠀⠀⠀⠀⠀⢸⠁]],
-    [[⠀⠀⡏⠀⣼⠀⢳⠊⠀⠀⠀⠀⠀⠀⠱⣀⣀⠔⣸⠁⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀]],
-    [[⠀⠀⡇⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀]],
-    [[⠀⢸⠃⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⢀⠀⠀⠀⠀⠀⣾⠀⠀]],
-    [[⠀⣸⠀⠀⠹⡄⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠸⠀⠀⠀⠀⠀⡇⠀⠀]],
-    [[⠀⡏⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀⢰⡀⠀⠀⠀⠀⠀⡇⠀⠀]],
-    [[⢰⠇⡄⠀⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣧⠀⠀]],
-    [[⣸⠀⡇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⢹⠀⠀⢸⠀⠀⢀⣿⠇⠀⠀⠀⠁⠀⢸⠀⠀]],
-    [[⣿⠀⡇⠀⠀⠀⠀⠀⢀⡤⠤⠶⠶⠾⠤⠄⢸⠀⡀⠸⣿⣀⠀⠀⠀⠀⠀⠈⣇⠀]],
-    [[⡇⠀⡇⠀⠀⡀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠸⡌⣵⡀⢳⡇⠀⠀⠀⠀⠀⠀⢹⡀]],
-    [[⡇⠀⠇⠀⠀⡇⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠮⢧⣀⣻⢂⠀⠀⠀⠀⠀⠀⢧]],
-    [[⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘]],
+local ascii = {
+    [1] = {
+        path = '~/.config/nvim/ascii/chika_fujiwara.txt',
+        height = 15,
+        width = 33,
+    },
+    [2] = {
+        path = '~/.config/nvim/ascii/anime_girl1.txt',
+        height = 20,
+        width = 30,
+    },
+    [3] = {
+        path = '~/.config/nvim/ascii/hydra.txt',
+        height = 11,
+        width = 32,
+    },
+    [4] = {
+        path = '~/.config/nvim/ascii/anime_girl2.txt',
+        height = 13,
+        width = 28,
+    },
+    [5] = {
+        path = '~/.config/nvim/ascii/anime_girl3.txt',
+        height = 14,
+        width = 30,
+    }
 }
+
+math.randomseed(os.time())
+local ascii_index = math.random(5)
+
+dashboard.preview_command = 'cat | lolcat -p 2'
+dashboard.preview_file_path = ascii[ascii_index].path
+dashboard.preview_file_height = ascii[ascii_index].height
+dashboard.preview_file_width = ascii[ascii_index].width
 
 dashboard.custom_center = {
     {
@@ -45,6 +58,3 @@ dashboard.custom_center = {
 }
 
 dashboard.custom_footer = {}
-
-dashboard.header_pad = 5
-dashboard.center_pad = 5
